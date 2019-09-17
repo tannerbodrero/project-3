f461e5ba5709c4ad2d431bde39b12bca762d0fd2
NelsonCad <NelsonCad97@gmail.com>
2019-09-17 12:32:45 -0700

    added bigger test

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,14 +4,27 @@ import React from "react"
 function home () {
     return (
         <section>
+
+            <article>
+                <p>
+                    to the winner go the spoils
+                </p>
+            </article>
+                {/* add comment of the ages here */}
             <div>
+                
+                <h1>
+                    Holy Toledo, 
+                    its the joker.
+                    ha ha hee hee hoo hoo.
+                    <h3>
+                        it ain't the joker tho.
+                    </h3>
+                </h1>
                 <h3>
                     And the turtle said quack.
                     413.
                 </h3>
-                <h1>
-                    Holy Toledo
-                </h1>
             </div>
         </section>
     )
