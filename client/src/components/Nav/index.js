8952dea6a0bd84cff234944947db9b90c449138e
Tanner Bodrero <tanner.bodrero@gmail.com>
2019-09-18 14:21:05 -0700

    Updating front end

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/home">Garage Trader</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/home"> View Offers</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/garage"> My Garage</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Log In</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
---
+++ client/src/pages/Splash.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Splash() {
+    return (
+        <section>
+            <h1>Welcome to Garage Trader!</h1>
+        </section>
+    );
+}
+
+export default Splash;
