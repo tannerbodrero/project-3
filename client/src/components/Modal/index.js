c8589d7328ac98fd8d78decae81ef809d9b0cfde
Gabriel <pedromurillo96@gmail.com>
2019-09-25 14:42:12 -0700

    view page created and updates

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -29,7 +29,8 @@ class ModalExample extends React.Component {
 
           </ModalBody>
           <ModalFooter className="footer-component">
-            <Button color="primary" onClick={this.props.newToggle}>Request exchange</Button>{' '}
+            <Button className="viewButton" href="/view" color="success" onClick={this.props.newToggle}>View more</Button>{' '}
+            <Button className="exchangeButton" color="primary" onClick={this.props.newToggle}>Request exchange</Button>{' '}
             <Button color="secondary" onClick={this.props.newToggle}>Cancel</Button>
           </ModalFooter>
         </Modal>
---
+++ client/src/pages/View.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Item from "../components/Item/Item";
+import ItemJumbotron from "../components/ItemJumbotron";
+import {  Form, FormGroup, Label, Input } from 'reactstrap';
+import ModalExample from "../components/Modal/index"
+import "./View.css";
+
+class View extends React.Component {
+
+    componentDidMount(props) {
+        this.loadView(this.props.email);
+    }
+
+      loadView = email => {
+        API.getItemsByName(email)
+          .then(res => this.setState({ items: res.data }))
+          .catch(err => console.log(err));
+      };
+
+    render() {
+        return (
+          <div>
+            
+
+        <h3 className="heading">
+            Welcome to the View page!
+        </h3>
+        <Form className="searchbar-wrap">
+        <FormGroup>
+          
+          
+        </FormGroup>
+      </Form>
+        
+        
+       
+        
+        <ItemJumbotron className="jumbo-background">
+        <div className="item-display-container">
+          
+        </div>
+        </ItemJumbotron>
+          </div>
+        );
+      }
+    }
+
+
+export default View;
